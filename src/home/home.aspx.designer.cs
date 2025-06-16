efa49fdcfef46a544d0ccd393ae9e0a548072121
Quang-25 <cohoi2512@gmail.com>
2025-06-16 19:44:37 +0700

    quang

--- src/home/home.aspx.designer.cs
+++ src/home/home.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace doan.src.home
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptSanPham;
+
+        /// <summary>
+        /// rptSanPhamMoi control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptSanPhamMoi;
     }
 }
